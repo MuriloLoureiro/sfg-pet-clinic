3f70e95b406880f28877aa0d5cfadcd3414f955b
murilo <murilo@DESKTOP-2URC4BA>
2019-09-24 22:37:25 -0300

    Criado os modelos básicos

---
+++ sfg-pet-clinic/src/main/java/guru/springframework/model/Person.java
@@ -0,0 +1,14 @@
+package guru.springframework.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Person {
+	
+	private String firstName;
+	private String lastName;
+	
+	
+}
---
+++ sfg-pet-clinic/src/main/java/guru/springframework/model/PetType.java
@@ -0,0 +1,11 @@
+package guru.springframework.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PetType {
+	
+	private String name;
+}
